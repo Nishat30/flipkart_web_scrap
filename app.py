c0e6b3a85b2856aad1c1cd4f37fca15ea76e8590
Nishat30 <nishatkhanam10prl@gmail.com>
2025-02-22 16:11:33 +0530

    first  commit

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import requests
 from bs4 import BeautifulSoup as bs
 from urllib.request import urlopen as uReq
 import logging
+import pymongo
 logging.basicConfig(filename="scrapper.log" , level=logging.INFO)
 
 app = Flask(__name__)
@@ -16,21 +17,24 @@ def homepage():
 def index():
     if request.method == 'POST':
         try:
-            searchString = request.form['content'].replace(" ","")
+            searchString = request.form['content'].replace(" ","") #replace a;l the space like iphone    7 is considered as iphone 7 only
             flipkart_url = "https://www.flipkart.com/search?q=" + searchString
             uClient = uReq(flipkart_url)
             flipkartPage = uClient.read()
             uClient.close()
             flipkart_html = bs(flipkartPage, "html.parser")
-            bigboxes = flipkart_html.findAll("div", {"class": "_1AtVbE col-12-12"})
+            bigboxes = flipkart_html.find_all("div", {"class": "cPHDOP col-12-12"})
             del bigboxes[0:3]
             box = bigboxes[0]
             productLink = "https://www.flipkart.com" + box.div.div.div.a['href']
-            prodRes = requests.get(productLink)
+            prodRes = requests.get(productLink, headers={"User-Agent": "Mozilla/5.0"})
+            if prodRes.status_code != 200:
+               print(f"Failed to fetch product page: {prodRes.status_code}")
+               return "Failed to fetch product details"
             prodRes.encoding='utf-8'
             prod_html = bs(prodRes.text, "html.parser")
             print(prod_html)
-            commentboxes = prod_html.find_all('div', {'class': "_16PBlm"})
+            commentboxes = prod_html.find_all('div', {'class': "RcXBOT"})
 
             filename = searchString + ".csv"
             fw = open(filename, "w")
@@ -40,15 +44,16 @@ def index():
             for commentbox in commentboxes:
                 try:
                     #name.encode(encoding='utf-8')
-                    name = commentbox.div.div.find_all('p', {'class': '_2sc7ZR _2V5EHH'})[0].text
+                    name = commentbox.find('p', {'class': '_2NsDsF AwS1CA'})
+                    name = name.text if name else "No Name"
 
                 except:
                     logging.info("name")
 
                 try:
                     #rating.encode(encoding='utf-8')
-                    rating = commentbox.div.div.div.div.text
-
+                    rating = commentbox.find("div", {"class": "XQDdHH Ga3i8K"})  # Updated class for ratings
+                    rating = rating.text if rating else "No Rating"
 
                 except:
                     rating = 'No Rating'
@@ -56,15 +61,13 @@ def index():
 
                 try:
                     #commentHead.encode(encoding='utf-8')
-                    commentHead = commentbox.div.div.div.p.text
+                    commentHead = commentbox.find("div", {"class": "z9E0IG"}).text
 
                 except:
                     commentHead = 'No Comment Heading'
                     logging.info(commentHead)
                 try:
-                    comtag = commentbox.div.div.find_all('div', {'class': ''})
-                    #custComment.encode(encoding='utf-8')
-                    custComment = comtag[0].div.text
+                    custComment = commentbox.find("div", {"class": "ZmyHeo"}).text
                 except Exception as e:
                     logging.info(e)
 
@@ -72,6 +75,12 @@ def index():
                           "Comment": custComment}
                 reviews.append(mydict)
             logging.info("log my final result {}".format(reviews))
+            
+            client= pymongo.MongoClient("mongodb+srv://nishatkhanam10prl:nishat30@cluster0.w7vmw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+            db=client['review_scrap']
+            review_col= db['review_scrap_data']
+            review_col.insert_many(reviews)
+            
             return render_template('result.html', reviews=reviews[0:(len(reviews)-1)])
         except Exception as e:
             logging.info(e)
